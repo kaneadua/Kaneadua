0ee4c166534470cff55da49f1785325b8f787c35
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2020-11-11 09:25:46 -0800

    homepage in progress

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -6,7 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
-
+  popularGames = [
+    {image: 'https://www.kaneadua.com/wp-content/uploads/2020/06/fifa20-vault-drop-article-featured-image.jpg.adapt_.crop16x9.431p.jpg',
+     title: 'FIFA 20'},
+     {image: 'https://www.kaneadua.com/wp-content/uploads/2020/06/fifa20-vault-drop-article-featured-image.jpg.adapt_.crop16x9.431p.jpg',
+     title: 'FIFA 20'},
+     {image: 'https://www.kaneadua.com/wp-content/uploads/2020/06/fifa20-vault-drop-article-featured-image.jpg.adapt_.crop16x9.431p.jpg',
+     title: 'FIFA 20'},
+     {image: 'https://www.kaneadua.com/wp-content/uploads/2020/06/fifa20-vault-drop-article-featured-image.jpg.adapt_.crop16x9.431p.jpg',
+     title: 'FIFA 20'},
+     {image: 'https://www.kaneadua.com/wp-content/uploads/2020/06/fifa20-vault-drop-article-featured-image.jpg.adapt_.crop16x9.431p.jpg',
+     title: 'FIFA 20'},
+     {image: 'https://www.kaneadua.com/wp-content/uploads/2020/06/fifa20-vault-drop-article-featured-image.jpg.adapt_.crop16x9.431p.jpg',
+     title: 'FIFA 20'},
+     {image: 'https://www.kaneadua.com/wp-content/uploads/2020/06/fifa20-vault-drop-article-featured-image.jpg.adapt_.crop16x9.431p.jpg',
+     title: 'FIFA 20'},
+     {image: 'https://www.kaneadua.com/wp-content/uploads/2020/06/fifa20-vault-drop-article-featured-image.jpg.adapt_.crop16x9.431p.jpg',
+     title: 'FIFA 20'},
+     {image: 'https://www.kaneadua.com/wp-content/uploads/2020/06/fifa20-vault-drop-article-featured-image.jpg.adapt_.crop16x9.431p.jpg',
+     title: 'FIFA 20'},
+     {image: 'https://www.kaneadua.com/wp-content/uploads/2020/06/fifa20-vault-drop-article-featured-image.jpg.adapt_.crop16x9.431p.jpg',
+     title: 'FIFA 20'},
+    ]
   constructor() { }
 
   ngOnInit(): void {
