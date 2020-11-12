e6ac64695344c019fd3ef4b6a7fced252dffcc9f
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2020-11-12 01:06:20 -0800

    homepage in progress

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
+  // dummy data for storing the games.. this will be done using a service and will be loaded from firebase
   popularGames = [
     {image: 'https://www.kaneadua.com/wp-content/uploads/2020/06/fifa20-vault-drop-article-featured-image.jpg.adapt_.crop16x9.431p.jpg',
      title: 'FIFA 20'},
