4862dad40c9f6b51e552dc1911129cc92d2cad38
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2020-11-24 18:02:21 -0800

    pages updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from './login/login.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BrowseGamesComponent } from './browse-games/browse-games.component';
 
 const routes: Routes = [
   {path: '', component: MainpageComponent, children: [
     {path: 'home', component: HomepageComponent},
     {path: 'register', component: RegisterComponent},
+    {path: 'browse-games', component: BrowseGamesComponent},
   ]},
   {path: 'login', component: LoginComponent}
 ];
