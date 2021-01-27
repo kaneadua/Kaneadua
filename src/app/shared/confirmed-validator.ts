c5f60eb62bad9be4984817ac5ccc1f8768331dec
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-01-27 03:18:52 -0800

    sign up page validation

---
+++ src/app/shared/confirmed-validator.ts
@@ -0,0 +1,39 @@
+import {AbstractControl} from '@angular/forms';
+
+export class PasswordValidation{
+
+    static MatchPassword(AC: AbstractControl){
+        let password = AC.get('password').value;
+        let confirmPassword = AC.get('confirmPassword').value;
+        if((password!= confirmPassword) || (password==''&& confirmPassword=='') ){
+            // console.log('false');
+            AC.get('confirmPassword').setErrors({MatchPassword: true});
+
+        }
+        else{
+            // console.log('true');
+            return null;
+        }
+    }
+
+    // static ValidCountry(AC: AbstractControl){
+    //     let country = AC.get('countryOfResidence').value;
+    //     console.log(country);
+    //     var verify = false;
+    //     let countries:any = document.getElementsByTagName('option');
+
+    // for (var i = 0; i < countries.length; i++) {
+    //     if (countries[i].value.toLowerCase() === country) {
+    //         verify = true;
+    //         break;
+    //     }
+
+    // }
+
+    // if (verify) {
+    //     return null;
+    // }else{
+    //     AC.get('countryOfResidence').setErrors({MatchedCountry: true});
+    // }
+    // }
+}
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,3 +1,5 @@
+import { PasswordValidation } from './../shared/confirmed-validator';
+import { escapeIdentifier } from '@angular/compiler/src/output/abstract_emitter';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
@@ -9,8 +11,9 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class SignUpComponent implements OnInit {
   signUpForm: FormGroup;
+  passwordError: boolean;
+  invalidUserName : boolean;
   userNameErrorMessage = "Username must begin with alphabet and may include numbers or hyphen";
-  emailErrorMessage = "Email is invalid";
   passwordErroMessage = "Password must be at least 8 characters";
   confirmPasswordErroMessage = "Both Passwords does not match";
   countryOfOriginErrorMessage = "you must select your country of origin";
@@ -26,12 +29,12 @@ export class SignUpComponent implements OnInit {
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('',[Validators.required, Validators.minLength(8)]),
       confirmPassword: new FormControl('',[Validators.required, Validators.minLength(8)]),
-      countryOfResidence: new FormControl('', Validators.required),
+      countryOfResidence: new FormControl('',[ Validators.required]),
       // checkboxes: requiredTrue
       newsLetter: new FormControl('', Validators.requiredTrue),
       termsAndConditions: new FormControl('',Validators.requiredTrue)
       
-    });
+    }, PasswordValidation.MatchPassword);
   }
 
   isValidInput(fieldName): boolean {
@@ -39,9 +42,29 @@ export class SignUpComponent implements OnInit {
       (this.signUpForm.controls[fieldName].dirty || this.signUpForm.controls[fieldName].touched);
    }
 
+  isInvalidInput(fieldName:string){
+    if(this.signUpForm.controls[fieldName].invalid){
+      this.signUpForm.controls[fieldName].markAsTouched();
+     }
+  }
+
   onSignUp(){
-    // this is where i put the javascript code
-    console.log(this.signUpForm);
+    if(this.signUpForm.valid==false){
+      console.log('Invalid input');
+      this.isInvalidInput('userName');
+      this.isInvalidInput('email');
+      this.isInvalidInput('password');
+      this.isInvalidInput('confirmPassword');
+      this.isInvalidInput('countryOfResidence');
+      this.isInvalidInput('newsLetter');
+      this.isInvalidInput('termsAndConditions');
+    }
+    else{
+      console.log(this.signUpForm);
+    }
+   
+
+  
   }
 
   onSubmit(){
