213cd590234b9178c2996b8e66520e919d503f16
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-02-03 02:06:16 -0800

    created a post.service.ts file that handles the http requests in order  to  communicate with the server

--- src/app/shared/post.service.ts
+++ src/app/shared/post.service.ts
@@ -8,11 +8,12 @@ export class PostService {
   url ="http://api.kaneadua.com/";
   constructor(private http: HttpClient) { }
 
-
+  // to get the details from the server
   getDetails(){
     return this.http.get(this.url);
   }
-  postDetails(userdetails:any){
-      return this.http.post(this.url,userdetails)
+  // to send post request to the server 
+  SendDetails(userdetails:any){
+      return this.http.post(this.url,userdetails);
   }
 }
