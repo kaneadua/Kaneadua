d25a9fdd2ad6f81851dada171f3fe7869b488c82
Paddy Pyker <paddypyker@pm.me>
2021-03-07 16:56:46 +0000

    Merge branch 'main' of https://github.com/kaneadua/Kaneadua into main

--- Community Page/index.js
+++
@@ -1,9 +0,0 @@
-function general_discussion_topics(number){
-	var a
-	if(number == 0) a= 'No topic yet'
-		else if (number == 1) a = number + ' topic'
-			else a= number + ' topics'
-
-	document.getElementById('GeneralDiscussion').innerHTML = a
-}  ///format the results appropriately
-
--- Community Page/old files/login.php
+++
@@ -1,6 +0,0 @@
-<?php 
- $db_hostname = 'localhost';
- $db_database = 'dictionary';
- $db_username = 'web'; 
- $db_password = 'website';
-?> 
